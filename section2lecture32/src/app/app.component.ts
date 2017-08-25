b4ba7f1feb45ad79b7582e6392a043354f81ac01
Roberto Juarez Maldonado <roberto.juarezmaldonado@soprasteria.com>
2017-08-25 12:24:36 +0200

    Add support for @Input attributes

--- section2lecture32/src/app/app.component.ts
+++ section2lecture32/src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+import {CardComponent} from './card/card.component'
+import {CardComponentModel} from './card-component-model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+  cardModels: Array< CardComponentModel > ;
+
+
+  constructor() {
+
+    this.initiateCards();
+  }
+
+
+  initiateCards() {
+
+    this.cardModels = [
+
+      new CardComponentModel( 'Usuarios', 'assets/img/users-icon.png' ),
+
+      new CardComponentModel( 'Entornos', 'assets/img/workspaces-icon.png' ),
+
+      new CardComponentModel( 'Listas de Distribución', 'assets/img/distribution-list-icon.png' )
+
+/*
+      new CardComponentModel( 'Campañas' ),
+
+      new CardComponentModel( 'Track de Mensajes' ),
+
+      new CardComponentModel( 'Log de Mensajes' )
+*/
+    ];
+  }
+
+
 }
---
+++ section2lecture32/src/app/card-component-model.ts
@@ -0,0 +1,16 @@
+export class CardComponentModel {
+
+
+  title: string;
+
+  imageSrc: string;
+
+
+  constructor( title: string, imageSrc: string ) {
+
+    this.title = title;
+
+    this.imageSrc = imageSrc;
+  }
+
+}
